71e027a4cdd156aae85ef5d2c0906bf78b00ee37
myselfAdul <159946150+myselfAdul@users.noreply.github.com>
2024-02-21 13:25:54 +0600

    Update Todo.jsx

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Todo = (props) => {
-    const {title, desc} = props.todo
-    const {id} = props
-    const date = new Date()
+  const { title, desc } = props.todo;
+  const { id } = props;
+  const date = new Date();
 
-    const cross = (id) =>{
-        props.handleDelete(id)
-        // console.log(id)
-        // alert(id)
-    }
-    
+  const cross = (id) => {
+    props.handleDelete(id);
+  };
+
   return (
     <div>
-        <article className='todo'>
-            <div className='shampoo'>
-                <h2>{title}</h2>
-                <p className='desc'>{desc}</p>
-            </div>
-            <div>
-                <button onClick={()=>{cross(id)}} className='dlt-btn'>X</button>
-            </div>
-            
-        </article>
-        <h5>{date.toLocaleDateString()}</h5>
-      
-    </div>
-  )
-}
+      <article className="todo">
+        <div className="shampoo">
+          <h2>{title}</h2>
 
-export default Todo
+         {/* Wrap the description in a card */}
+            <p className='desc'>{desc}</p>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              cross(id);
+            }}
+            className="dlt-btn"
+          >
+            X
+          </button>
+        </div>
+      </article>
+      <h5>{date.toLocaleDateString()}</h5>
+    </div>
+  );
+};
+
+export default Todo;
